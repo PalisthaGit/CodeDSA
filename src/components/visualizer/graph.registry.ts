e3f4b3c6b2d9df56126f70154823b4efee0c0f9a
Palistha <palistha@Palisthas-MacBook-Air.local>
2026-07-09 18:20:01 +0545

    dijkstras

--- src/components/visualizer/graph.registry.ts
+++ src/components/visualizer/graph.registry.ts
@@ -2,11 +2,13 @@ import type { GraphAlgorithmDefinition } from './graph.types'
 import { definition as astar } from './graph-algos/astar'
 import { definition as bfs } from './graph-algos/bfs'
 import { definition as dfs } from './graph-algos/dfs'
+import { definition as dijkstras } from './graph-algos/dijkstra'
 import { definition as prims } from './graph-algos/prim'
 
 export const graphRegistry: Record<string, GraphAlgorithmDefinition> = {
   [astar.key]: astar,
   [bfs.key]: bfs,
   [dfs.key]: dfs,
+  [dijkstras.key]: dijkstras,
   [prims.key]: prims,
 }
